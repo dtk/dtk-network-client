db84ee27866bd30669aecd7f91ed88bda9315171
Aldin Milanovic <aldin@atlantbh.com>
2017-11-28 13:49:15 +0100

    Handle error messages

--- lib/client/error.rb
+++ lib/client/error.rb
@@ -1,7 +1,14 @@
 module DTK::Network::Client
   class Error < NameError
     def initialize(msg = '')
-      super(msg)
+      if errors = msg['errors']
+      error_msg = ''
+      # error_msg << "#{errors['code'].upcase} " if errors['code']
+      error_msg << errors['message'] if errors['message']
+      super(error_msg)
+      else
+        super(msg)
+      end
     end
   end
 end
