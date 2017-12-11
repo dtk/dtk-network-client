f4c30728b7a386b99ff1e3a3463d1d3b09d5cc06
Aldin Milanovic <aldin@atlantbh.com>
2017-12-11 13:06:11 +0100

    Add dtk module dtkn support to list remote modules

--- lib/client/command/list.rb
+++ lib/client/command/list.rb
@@ -1,10 +1,12 @@
 module DTK::Network::Client
   class Command
     class List < self
-      def self.run(opts = {})
-        modules_info = rest_get('modules')
-        modules_info.merge!("status" => "ok", "datatype" =>"remote_module")
-        modules_info
+      def self.run(namespace, opts = {})
+        params = namespace ? {namespace: namespace} : {}
+        modules_info = rest_get('modules', params)
+
+        # return format expected by dtk-client, should change this to be more generic
+        {'status' => 'ok', 'datatype' => 'remote_module', 'data' => modules_info}
       end
     end
   end
