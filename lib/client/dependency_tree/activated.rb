171afc2295f761627e47a7cd45bf4bfd322f6f38
Aldin Milanovic <aldin@atlantbh.com>
2017-12-21 15:50:02 +0100

    Small change

--- lib/client/dependency_tree/activated.rb
+++ lib/client/dependency_tree/activated.rb
@@ -5,13 +5,13 @@ module DTK::Network
         def module_activated?(dependency)
           if existing_dep = self["#{dependency.full_name}"]
             if required_version = dependency.version
-              required_version.satisfied_by?(existing_dep[:version])
+              required_version.satisfied_by?(existing_dep[:version] || existing_dep['version'])
             end
           end
         end
 
         def add!(dependency_mod)
-          self.merge!("#{dependency_mod.full_name}" => { version: dependency_mod.version.str_version })
+          self.merge!("#{dependency_mod.full_name}" => { 'version' => dependency_mod.version.str_version })
         end
 
         def existing_name?(name)
