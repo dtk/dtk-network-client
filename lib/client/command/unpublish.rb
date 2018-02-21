a8c02c48d4dd6fdb6ed38d0d4c34b059fc1d59d6
Aldin Milanovic <aldin@atlantbh.com>
2018-02-21 16:57:42 +0100

    Delete branch from s3 and codecommit

--- lib/client/command/unpublish.rb
+++ lib/client/command/unpublish.rb
@@ -18,27 +18,6 @@ module DTK::Network::Client
           namespace: @module_ref.namespace,
           version: version.str_version
         }
-
-        if version.is_semantic_version?
-          delete_info = rest_get("modules/delete_info", params)
-          delete_creds = delete_info['delete_credentails']
-          bucket, object_name = S3Helper.ret_s3_bucket_info(delete_info)
-
-          s3_args = Args.new({
-            region: 'us-east-1',
-            access_key_id: delete_creds['access_key_id'],
-            secret_access_key: delete_creds['secret_access_key'],
-            session_token: delete_creds['session_token']
-          })
-          storage = Storage.new(:s3, s3_args)
-
-          delete_args = Args.new({
-            bucket: bucket,
-            key: object_name
-          })
-          storage.delete(delete_args)
-        end
-
         rest_post("modules/unpublish", params)
 
         nil
